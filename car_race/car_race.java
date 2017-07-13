7a2816944cb0366c708a4fc55473608c072b3da2
Gabriele Ficara <Gabbo@iMacdiGleFicara.homenet.telecomitalia.it>
2017-07-13 18:53:39 +0200

    aggiunta road e inizio main

--- car_race/car_race.java
+++ car_race/car_race.java
@@ -12,17 +12,45 @@ public	static	class car{
 
 	}
 
-	public	int	main (){
-	
+	public int getvel(){
+		return vel;
+	}
+
+	public	static main (){
+		int i = 0;
+		int j;
+		int str[100];
+
+		ciro = new car("ciruzzo", 5, 4);
+		newroad = new road(str);
+		
+		while (i < 100){
+			j = 0;
+			while (j < ciro.getvel || i + j < 100){
+				
+				j++;
+			}
+			i = i + j;
+		}
 	}
 }
 
-public static road{
+public static class road{
 
-	int	[100];
-	int	i = 0;
+	int	pos[100];
 
-	while {
-		
+	public static road (int newpos[100]){
+	
+		int	i = 0;
+
+		while(i < 100){
+			i = 1;
+			i++;
+		}
+		i = 0;
+		while (i < 5){
+			newpos[((int)(math.random() * 100))] = 0;
+			i++;
+		}
 	}
 }
