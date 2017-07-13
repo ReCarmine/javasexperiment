42bed1793ebcdcad9252260e8ca2d64c21d4b5d3
Gabriele Ficara <Gabbo@iMacdiGleFicara.homenet.telecomitalia.it>
2017-07-13 17:19:57 +0200

    car definition

---
+++ car_race/car_race.java
@@ -0,0 +1,28 @@
+public	static	class car{
+
+	string	model;
+	int	vel;
+	int	acc;
+
+	public	static car (string newmodel, int newvel, int newacc){
+	
+		model = newmodel;
+		vel = newvel;
+		acc = newacc;
+
+	}
+
+	public	int	main (){
+	
+	}
+}
+
+public static road{
+
+	int	[100];
+	int	i = 0;
+
+	while {
+		
+	}
+}
